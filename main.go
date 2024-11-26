bb55bd0d1e6a2c9dc9b50f21a5328c7ac85b3e86
sooncheewong21@gmail.com <sooncheewong21@gmail.com>
2024-11-26 16:00:32 +0800

    modify main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"os"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -13,15 +12,11 @@ import (
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name st-tencentcloud
 
 func main() {
-	var debug bool
-	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
-	flag.Parse()
 	providerAddress := os.Getenv("PROVIDER_LOCAL_PATH")
 	if providerAddress == "" {
 		providerAddress = "registry.terraform.io/myklst/st-tencentcloud"
 	}
 	providerserver.Serve(context.Background(), tencentcloud.New, providerserver.ServeOpts{
 		Address: providerAddress,
-		Debug:   debug,
 	})
 }
